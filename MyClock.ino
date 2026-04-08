c9cc1b8cb769237a63c6390ea22668ccc4e80deb
paIT-create <kontakt.pk@outlook.com>
2026-04-08 16:49:02 +0200

    Update MyClock.ino

--- MyClock.ino
+++ MyClock.ino
@@ -32,6 +32,8 @@
 // ===== Preferences =====
 #include <Preferences.h>
 
+portMUX_TYPE displayMux = portMUX_INITIALIZER_UNLOCKED;
+
 // -----------------------------------------------------------------------------
 // Pinout (YOUR PROVIDED PINS)
 // -----------------------------------------------------------------------------
@@ -222,12 +224,12 @@ void setDisplayDashes() {
 }
 
 void commitDisplayBuffer() {
-  taskENTER_CRITICAL();
+  portENTER_CRITICAL(&displayMux);
   g_displaySeg[0] = g_displayNext[0];
   g_displaySeg[1] = g_displayNext[1];
   g_displaySeg[2] = g_displayNext[2];
   g_displaySeg[3] = g_displayNext[3];
-  taskEXIT_CRITICAL();
+  tportEXIT_CRITICAL(&displayMux);
 }
 
 // -----------------------------------------------------------------------------
